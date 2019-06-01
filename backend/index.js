ef68c0a65fbe81a8c8cd82a865c1d56639d72ca1
Jackson Silva <jackson.rodrigues9@hotmail.com>
2019-06-01 19:25:18 -0300

    Utilizando consign para dependencia de arquivos

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,9 @@
 // Criando o express
 const app = require('express')();
+const consign = require('consign')
+
+// Consign é usado para ajudar nas dependências dos arquivos com as funções
+consign().then('./config/middlewares.js').into(app)
 
 // Iniciando na porta 300
 app.listen(3000, () => {
