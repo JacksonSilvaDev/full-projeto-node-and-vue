e5356249bad3e41f08f83b5e5ebbac65b77e8ed6
Jackson Silva <jackson.rodrigues9@hotmail.com>
2019-06-01 20:46:27 -0300

    Criando as tabelas de nosso sistema

--- backend/migrations/20190601200002_create_table_users.js
+++ backend/migrations/20190601200002_create_table_users.js
@@ -1,8 +1,19 @@
 
 exports.up = function(knex, Promise) {
-  
+    // Criando um schema de table users
+    return knex.schema.createTable('users', table => {
+        // Colocando o id como chave primária
+        table.increments('id').primary()
+        // Setando que ela não pode ser nullo
+        table.string('name').notNull()
+        // Setando que o valor email sera unico
+        table.string('email').notNull().unique()
+        table.string('password').notNull()
+        // Setando valor padrão de inicio
+        table.boolean('admin').notNull().defaultTo(false)
+    })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('users')
 };
