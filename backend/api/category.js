cb9852946253bf81723c870e90702a1cbb8e8c2f
Jackson Silva <jackson.rodrigues9@hotmail.com>
2019-06-01 21:37:42 -0300

    Buscando a categoria pai de cada category

--- backend/api/category.js
+++ backend/api/category.js
@@ -51,4 +51,32 @@ module.exports = app => {
             res.status(400).send(err)
         }
     }
+
+    const withPath = categories => {
+        const getParent = (categories, parentId) => {
+            const parent = categories.filter(category => category.id === parentId)
+
+            return parent.length ? parent[0] : null
+        }
+
+        const categoriesWithPath = categories.map(category => {
+            let path = category.name
+            let parent = getParent(categories, category.parentId)
+
+            while(parent) {
+                path = `${parent.name} > ${path}`
+                parent = getParent(categories, category.parentId)
+            }
+
+            return {... category, path}
+        })
+
+        categoriesWithPath.sort((a,b) => {
+            if(a.path < b.path) return -1
+            if(a.path > b.path) return 1
+            return 0
+        })
+
+        return categoriesWithPath
+    }
 }
